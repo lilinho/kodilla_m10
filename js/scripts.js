50f3795fc12f0f2b85da969cb23c6b55949c5b9a
Tomasz Pawelec <30554524+lilinho@users.noreply.github.com>
2017-11-11 00:43:11 +0100

    indicators are working. need to add onclick action

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,11 @@ $(function() {
   
     var width = 720,
         speed = 500,
-        currentSlide = 0,
+        currentSlide = 1,
         interval;
     var sliderContainer = $("#carousel .slides");
     var indicatorsContainer = $("#indicators");
+    var indicator = $(".indicator", indicatorsContainer);
     
     function moveFirstSlide() {
         var first = sliderContainer.find(".slide:first"),
@@ -15,8 +16,16 @@ $(function() {
         last.after(first);
         sliderContainer.css({"marginLeft":0})
     }
+    
     function changeSlide() {
         sliderContainer.animate({'marginLeft' : '-='+width}, speed, moveFirstSlide);
+        if(++currentSlide == (indicator.length + 1)){
+            $(".indicator:nth-child(" + (currentSlide - 1) + ")").css('opacity', 0.3);
+            currentSlide = 1;
+            $(".indicator:nth-child(" + currentSlide + ")").css('opacity', 1.0);
+        }
+        $(".indicator:nth-child(" + (currentSlide - 1) + ")").css('opacity', 0.3);
+        $(".indicator:nth-child(" + currentSlide + ")").css('opacity', 1.0);
       
     }
     function startSlider() {
@@ -31,6 +40,12 @@ $(function() {
     
     sliderContainer
         .on('mouseenter', stopSlider)
-        .on('mouseleave', startSlider)
-    startSlider()
+        .on('mouseleave', startSlider);
+    indicator.click(function() {
+        var first = sliderContainer.find(".slide:nth-child(" + indicatorsContainer.index(this)  + ")");
+        var last = sliderContainer.find(".slide:last");
+        last.after(first);
+        sliderContainer.css({"marginLeft":0});
+    });
+    startSlider();
 })
