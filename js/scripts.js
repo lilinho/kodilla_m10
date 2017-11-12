2b206a69dcedb56ae3af68a5b249dd5279fc5409
Tomasz Pawelec <tpawelec@outlook.com>
2017-11-12 10:18:06 +0100

    everything works. I think

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
+
+/*
+jQuery slider. No RWD (for now)
+*/
+
 'use strict';
 
 $(function() {
   
+    //slider settings
     var width = 720,
         speed = 500,
         currentSlide = 1,
         interval;
+
+    //DOM cache    
     var sliderAll = $("#carousel");
     var sliderContainer = $("#carousel .slides");
     var indicatorsContainer = $("#indicators");
     var indicator = $(".indicator", indicatorsContainer);
     
+    /*
+    Function, which moves last slide to first position and resets marginLeft property.
+    In other words - thanks to this function we see always first slide
+    */
     function moveFirstSlide() {
         var first = sliderContainer.find(".slide:first"),
             last = sliderContainer.find(".slide:last");
@@ -18,6 +30,7 @@ $(function() {
         sliderContainer.css({"marginLeft":0})
     }
     
+    // reverse than moveFirstSlide. Only for left arrow 
     function moveLastSlide() {
         var first = sliderContainer.find(".slide:first"),
             last = sliderContainer.find(".slide:last");
@@ -25,20 +38,34 @@ $(function() {
         sliderContainer.css({"marginLeft":-(width)})
     }
     
-    function changeSlide(target, sp) {
-        sliderContainer.animate({'marginLeft' : '-='+target*width}, sp, moveFirstSlide);
+    /*
+    It highlights actual indicator. Hot it works is - index of displayed slide is stored
+    in currentSlide variable.
+    1. It changes opacity of its indicator
+    2. Writes to currentSlide index of current slide 
+    (Let's say second slide is displayed. User clicked on 4th indicator, so 4th slide is displayed
+    So current slide is 2 + 2 = 4 (target is how many slides it have to skip) 
+    4 % 5 (number of slides) = 4. So 4th indicator will bi highlighted. I works also when
+    user will click indicator that is to the left of current highlighted indicator)
+    */
+    function changeIndicator(target) {
         $(".indicator:nth-child(" + currentSlide + ")").css('opacity', 0.3);
         currentSlide = (currentSlide + target) % $('.slide', sliderContainer).length;
         if (currentSlide == 0) {
             currentSlide = $('.slide', sliderContainer).length;
         }
         $(".indicator:nth-child(" + currentSlide + ")").css('opacity', 1.0);
+    }
+
+    function changeSlide(target) {
+        sliderContainer.animate({'marginLeft' : '-='+target*width}, speed, moveFirstSlide);
+        changeIndicator(target);
               
     }
     
     function startSlider() {
         
-        interval = setInterval(function() {changeSlide(1, speed)}, 2000);
+        interval = setInterval(function() {changeSlide(1)}, 2000);
         
     }
     
@@ -56,22 +83,23 @@ $(function() {
             target = target - currentSlide;
             for(var i = 1; i < target; i++)
                 moveFirstSlide();
-            changeSlide(target, speed);
+            changeSlide(target);
         } else if (target < currentSlide) {
             target = currentSlide + target;
             for(var i = 1; i < (target - currentSlide); i++)
                 moveFirstSlide();
-            changeSlide(target, speed);
+            changeSlide(target);
         }
         
     });
     
     $('#arrow-right').click(function() {
-        changeSlide(1, speed);
+        changeSlide(1);
     });
     $('#arrow-left').click(function() {
         moveLastSlide();
-        sliderContainer.animate({'marginLeft' : '+='+width}, speed, moveFirstSlide);
+        sliderContainer.animate({'marginLeft' : '+='+width}, speed);
+        changeIndicator(-1);
     });
     startSlider();
 })
